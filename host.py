4685ddbd3975fd7aefc654816089cbc4195356e2
PelinalWS <batucan971@gmail.com>
2024-05-31 09:46:13 +0300

    final

--- host.py
+++ host.py
@@ -41,7 +41,8 @@ if st.button('Enter Data Manually'):
     euribor3m = st.number_input('Euribor 3 Month Rate')
     nr_employed = st.number_input('Number of Employees')
     showPredict = True
-elif st.button('Enter CSV Line'):
+    
+if st.button('Enter CSV Line'):
     csv_input = st.text_input('Enter values separated by semicolons:')
     if csv_input:
             try:
@@ -72,36 +73,36 @@ elif st.button('Enter CSV Line'):
                     st.error(f"Please enter exactly {len(data)} values.")
             except ValueError:
                 st.error("Please enter valid values separated by semicolons.")
-
-if st.button('Predict'):
-    input_data = pd.DataFrame({
-    'age': [age],
-    'job': [job],
-    'marital': [marital],
-    'education': [education],
-    'default': [default],
-    'housing': [housing],
-    'loan': [loan],
-    'contact': [contact],
-    'month': [month],
-    'day_of_week': [day_of_week],
-    'duration': [duration],
-    'campaign': [campaign],
-    'pdays': [pdays],
-    'previous': [previous],
-    'poutcome': [poutcome],
-    'emp.var.rate': [emp_var_rate],
-    'cons.price.idx': [cons_price_idx],
-    'cons.conf.idx': [cons_conf_idx],
-    'euribor3m': [euribor3m],
-    'nr.employed': [nr_employed]
-    })
-    prediction = model.predict(input_data)
-    st.subheader('Prediction')
-    if(prediction[0] == 1):
-        st.write("The model predicts that you subscribed!")
-    else:
-        st.write("The model predicts that you didn't subscribe.")
+if show_predict:
+    if st.button('Predict'):
+        input_data = pd.DataFrame({
+        'age': [age],
+        'job': [job],
+        'marital': [marital],
+        'education': [education],
+        'default': [default],
+        'housing': [housing],
+        'loan': [loan],
+        'contact': [contact],
+        'month': [month],
+        'day_of_week': [day_of_week],
+        'duration': [duration],
+        'campaign': [campaign],
+        'pdays': [pdays],
+        'previous': [previous],
+        'poutcome': [poutcome],
+        'emp.var.rate': [emp_var_rate],
+        'cons.price.idx': [cons_price_idx],
+        'cons.conf.idx': [cons_conf_idx],
+        'euribor3m': [euribor3m],
+        'nr.employed': [nr_employed]
+        })
+        prediction = model.predict(input_data)
+        st.subheader('Prediction')
+        if(prediction[0] == 1):
+            st.write("The model predicts that you subscribed!")
+        else:
+            st.write("The model predicts that you didn't subscribe.")
 
 
 
