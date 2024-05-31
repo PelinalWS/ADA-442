1679d00e331c149af320b1ce31a334676e90f609
PelinalWS <batucan971@gmail.com>
2024-05-31 10:05:22 +0300

    ab

--- host.py
+++ host.py
@@ -20,30 +20,29 @@ st.title("Term Deposit Subscription Guesser")
 showPredict = False
 getInput = False
 
-if st.button('Enter Data Manually'):
-    age = st.number_input('Age', min_value=0)
-    job = st.selectbox('Job', jobs)
-    marital = st.selectbox('Marital Status', marital_status)
-    education = st.selectbox('Education level', education)
-    default = st.selectbox('Credit in Default', default)
-    housing = st.selectbox('Housing Loan', housing)
-    loan = st.selectbox('Personal Loan', loan)
-    contact = st.selectbox('Contact', contact)
-    month = st.selectbox('Month', months)
-    day_of_week = st.selectbox('Day of Week', days)
-    duration = st.number_input('Duration of Last Contact', min_value=0)
-    campaign = st.number_input('Contacts During Campaign', min_value=0)
-    pdays = st.number_input('#Days Passed After Last Contact', min_value=0)
-    previous = st.number_input('#Previously Contacted', min_value=0)
-    poutcome = st.selectbox('Previous Campaign Outcome', poutcome)
-    emp_var_rate = st.number_input('Employment Variation Rate')
-    cons_price_idx = st.number_input('Consumer Price Index')
-    cons_conf_idx = st.number_input('Consumer Confidence Index')
-    euribor3m = st.number_input('Euribor 3 Month Rate')
-    nr_employed = st.number_input('Number of Employees')
-    showPredict = True
+st.subheader('Enter Data Manually')
+age = st.number_input('Age', min_value=0)
+job = st.selectbox('Job', jobs)
+marital = st.selectbox('Marital Status', marital_status)
+education = st.selectbox('Education level', education)
+default = st.selectbox('Credit in Default', default)
+housing = st.selectbox('Housing Loan', housing)
+loan = st.selectbox('Personal Loan', loan)
+contact = st.selectbox('Contact', contact)
+month = st.selectbox('Month', months)
+day_of_week = st.selectbox('Day of Week', days)
+duration = st.number_input('Duration of Last Contact', min_value=0)
+campaign = st.number_input('Contacts During Campaign', min_value=0)
+pdays = st.number_input('#Days Passed After Last Contact', min_value=0)
+previous = st.number_input('#Previously Contacted', min_value=0)
+poutcome = st.selectbox('Previous Campaign Outcome', poutcome)
+emp_var_rate = st.number_input('Employment Variation Rate')
+cons_price_idx = st.number_input('Consumer Price Index')
+cons_conf_idx = st.number_input('Consumer Confidence Index')
+euribor3m = st.number_input('Euribor 3 Month Rate')
+nr_employed = st.number_input('Number of Employees')
     
-st.subheader('Enter CSV Line'):
+st.subheader('Enter CSV Line')
 csv_input = st.text_input('Enter values separated by semicolons:')
 if csv_input:
     try:
@@ -69,42 +68,37 @@ if csv_input:
             cons_conf_idx = float(csv_values[17])
             euribor3m = float(csv_values[18])
             nr_employed = float(csv_values[19])
-            showPredict = True
         else:
             st.error(f"Please enter exactly 20 values.")
     except ValueError:
         st.error("Please enter valid values separated by semicolons.")
 
-if showPredict:
-    if st.button('Predict'):
-        input_data = pd.DataFrame({
-        'age': [age],
-        'job': [job],
-        'marital': [marital],
-        'education': [education],
-        'default': [default],
-        'housing': [housing],
-        'loan': [loan],
-        'contact': [contact],
-        'month': [month],
-        'day_of_week': [day_of_week],
-        'duration': [duration],
-        'campaign': [campaign],
-        'pdays': [pdays],
-        'previous': [previous],
-        'poutcome': [poutcome],
-        'emp.var.rate': [emp_var_rate],
-        'cons.price.idx': [cons_price_idx],
-        'cons.conf.idx': [cons_conf_idx],
-        'euribor3m': [euribor3m],
-        'nr.employed': [nr_employed]
-        })
-        prediction = model.predict(input_data)
-        st.subheader('Prediction')
-        if(prediction[0] == 1):
-            st.write("The model predicts that you subscribed!")
-        else:
-            st.write("The model predicts that you didn't subscribe.")
-
-
-
+if st.button('Predict'):
+    input_data = pd.DataFrame({
+    'age': [age],
+    'job': [job],
+    'marital': [marital],
+    'education': [education],
+    'default': [default],
+    'housing': [housing],
+    'loan': [loan],
+    'contact': [contact],
+    'month': [month],
+    'day_of_week': [day_of_week],
+    'duration': [duration],
+    'campaign': [campaign],
+    'pdays': [pdays],
+    'previous': [previous],
+    'poutcome': [poutcome],
+    'emp.var.rate': [emp_var_rate],
+    'cons.price.idx': [cons_price_idx],
+    'cons.conf.idx': [cons_conf_idx],
+    'euribor3m': [euribor3m],
+    'nr.employed': [nr_employed]
+    })
+    prediction = model.predict(input_data)
+    st.subheader('Prediction')
+    if(prediction[0] == 1):
+        st.write("The model predicts that you subscribed!")
+    else:
+        st.write("The model predicts that you didn't subscribe.")
